4bd59244f9a32b42995bd4b7049e466411f3ef83
Jordan Sne <jsne10@live.ca>
2013-04-12 13:41:00 -0400

    Minor code fixes

--- src/main/java/com/jsne10/nodrops/NoDrops.java
+++ src/main/java/com/jsne10/nodrops/NoDrops.java
@@ -1,21 +1,31 @@
 package com.jsne10.nodrops;
 
 import com.jsne10.nodrops.listeners.*;
+import com.jsne10.nodrops.util.Metrics;
 
 import java.io.IOException;
 
+import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import com.jsne10.nodrops.util.Metrics;
 
 public class NoDrops extends JavaPlugin {
 	
 	public static NoDrops plugin;
+	
+	private Configuration config;
+	private FileConfiguration configFile;
 
 	@Override
-	public void onEnable() {
+	public void onLoad() {
 		
 		plugin = this;
+		configFile = getConfig();
+		
+	}
+	
+	@Override
+	public void onEnable() {
 
 		// Registers the Drop listener events.
 		this.getServer().getPluginManager().registerEvents(new DropsDisable(), this);
@@ -25,9 +35,9 @@ public class NoDrops extends JavaPlugin {
 		try {
 		    Metrics metrics = new Metrics(this);
 		    metrics.start();
-		    this.getLogger().info("Successfully connected to Plugin Metrics.");
+		    this.getLogger().info("Successfully hooked to Plugin Metrics.");
 		} catch (IOException e) {
-		    this.getLogger().warning("Failed to connect to Plugin Metrics");
+		    this.getLogger().warning("Failed to hook to Plugin Metrics");
 		}
 
 	}
