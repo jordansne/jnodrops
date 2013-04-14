74a6d32d45ccf8da550a284e52f36a8c498fd93d
jsne10 <jsne10@live.ca>
2013-04-14 17:11:23 -0300

    Update DropsDisable.java

--- src/main/java/com/jsne10/nodrops/listeners/DropsDisable.java
+++ src/main/java/com/jsne10/nodrops/listeners/DropsDisable.java
@@ -9,15 +9,10 @@ public class DropsDisable implements Listener {
 
 	@EventHandler
 	public void onBlockDrop(PlayerDropItemEvent event) {
-		if (!event.getPlayer().hasPermission("jnodrops.candrop")) {
-			
-			if (!event.getPlayer().hasPermission("jnodrops.candrop." + event.getPlayer().getWorld().getName())) {
+		if (!event.getPlayer().hasPermission("jnodrops.candrop") && !event.getPlayer().hasPermission("jnodrops.candrop." + event.getPlayer().getWorld().getName())) {
 				event.setCancelled(true);
 				event.getPlayer().sendMessage(ChatColor.RED + "[NoDrops] " + ChatColor.GRAY + "You must not share items!");
 				return;
-			} else {
-				return;
-			}
 			
 		}
 	}
