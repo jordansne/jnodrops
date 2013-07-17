eb56dcc188c801e0e5814c8d65787c234df6050f
Jordan Sne <jsne10@live.ca>
2013-07-17 13:01:40 -0400

    Started working on automatic config updater.

---
+++ src/main/java/com/jsne10/nodrops/util/ConfigUpdater.java
@@ -0,0 +1,19 @@
+package com.jsne10.nodrops.util;
+
+public class ConfigUpdater {
+	
+	public ConfigUpdater() {
+		
+	}
+	
+	public void checkIfOutdated() {
+		if (true) {
+			this.update();
+		}
+	}
+	
+	private void update() {
+		
+	}
+
+}
