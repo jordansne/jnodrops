a30b48444cf94df74a2e5a9f2922b4b7e77e4698
Jordan Sne <jsne10@live.ca>
2013-07-21 22:07:43 -0400

    Fixed Code Issues #sweg

--- src/main/java/com/jsne10/nodrops/event/PotionDrinkDespawner.java
+++ src/main/java/com/jsne10/nodrops/event/PotionDrinkDespawner.java
@@ -1,6 +1,7 @@
 package com.jsne10.nodrops.event;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
@@ -16,12 +17,34 @@ public class PotionDrinkDespawner implements Listener {
 				Material mat = event.getItem().getType();
 
 				if (mat == Material.POTION) {
-					event.setCancelled(true);
+					new PotionThread(event.getPlayer()).start();
 				}
 				
 			} catch (Exception e) {}
 		}
 
 	}
-
+	
+	private class PotionThread extends Thread {
+		
+		private Player player;
+		
+		public PotionThread(Player player) {
+			super();
+			this.player = player;
+		}
+		
+		@Override
+		public void run() {
+			try {
+				sleep(300);
+				
+				if (player.getItemInHand().getType() == Material.GLASS_BOTTLE) {
+					player.setItemInHand(null);
+				}
+				
+			} catch (InterruptedException e) {}
+		}
+		
+	}
 }
