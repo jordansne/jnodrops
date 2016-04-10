dca0ead23b66d12fd0101927f119d51416e6f2ff
Jordan <jsne10@live.ca>
2016-04-10 17:34:31 -0400

    License Fixed
    Updated existing license headers & added missing ones.

--- src/main/java/com/jsne10/jnodrops/JNoDrops.java
+++ src/main/java/com/jsne10/jnodrops/JNoDrops.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 jsne10.  All rights reserved.
+ * Copyright (C) 2013-2016 jsne10.  All rights reserved.
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
